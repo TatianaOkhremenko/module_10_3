e5f5f50dc5a40cb76c22a3a888300a2e54b251ed
TatianaOkhremenko <tafinka@mail.ru>
2024-11-05 17:54:20 +0300

    Update module_10_3.py

--- module_10_3.py
+++ module_10_3.py
@@ -1,45 +1,57 @@
 import threading
 import time
-from random import randint
+import random
+from threading import Thread, Lock
 
 
 class Bank:
+
     def __init__(self):
         self.balance = 0
-        self.lock = threading.Lock()
+        self.lock = Lock()
 
     def deposit(self):
-        for i in range(100):
-            replenishment_deposit = randint(50, 500)
-            with self.lock:
-                if self.balance <= 500 and self.lock.locked():
-                    self.balance += replenishment_deposit
-                    print(f'Пополнение: {replenishment_deposit}. Баланс: {self.balance}')
+        count_iter = 0
+        while count_iter <= 99:
+            bk = random.randint(50, 500)
+            self.lock.acquire()
 
-            time.sleep(0.001)
+            try:
+                self.balance += bk
+                print(f'Пополнение: {bk}. Баланс: {self.balance}\n')
+            finally:
+                self.lock.release()
+            time.sleep(0.01)
+            count_iter += 1 
 
     def take(self):
-        for i in range(100):
-
-            take_money = randint(50, 500)
-            print(f'Запрос на {take_money}')
-            with self.lock:
-                if take_money <= self.balance:
-                    self.balance -= take_money
-                    print(f'Снятие: {take_money}. Баланс: {self.balance}')
+        count_iter = 0
+        while count_iter <= 99:
+            bk = random.randint(50, 500)
+            print(f'Запрос на {bk}')
+            self.lock.acquire()
+            try:
+                if bk <= self.balance:
+                    self.balance -= bk
+                    print(f'Снятие: {bk}. Баланс: {self.balance}\n')
                 else:
-                    print(f'Запрос отклонен, недостаточно средств')
-
-            time.sleep(0.001)
+                    print('Запрос отклонён, недостаточно средств')
+            finally:
+                self.lock.release()
+            time.sleep(0.01)
+            count_iter += 1
 
 
 bk = Bank()
 
-th1 = threading.Thread(target=Bank.deposit, args=(bk,))
-th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+th1 = threading.Thread(target=bk.deposit)
+th2 = threading.Thread(target=bk.take)
 
 th1.start()
 th2.start()
+
+
 th1.join()
 th2.join()
 
